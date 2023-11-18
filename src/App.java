97607875ad69d2ad387abe074f559e3c6be88968
laptop 4 <frc7403@columbus.edu.co>
2023-11-18 11:03:09 -0500

    Nicolas Henao: Hola

---
+++ src/App.java
@@ -0,0 +1,21 @@
+import javax.swing.*;
+import java.awt.*;
+
+
+public class App {
+
+    public static JFrame frame;
+    public static JPanel panel;
+    public static void main(String... args) throws Exception {
+       /*  frame = new JFrame();
+        panel = new JPanel();
+
+        frame.add(panel);
+
+        frame.setVisible(true);
+        frame.setSize(new Dimension(300,300));
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        */
+        
+    }
+}
